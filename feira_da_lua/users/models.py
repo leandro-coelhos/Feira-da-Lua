0d33424f7539c7f0857b48a3e56eee65f5554ced
Erik <erikcojho@gmail.com>
2025-11-21 20:20:33 -0300

    Adiciona models de User e Marketplace certo

--- feira_da_lua/users/models.py
+++ feira_da_lua/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    complete_name = models.CharField(max_length=255)
+
+    # Campos padrões do AbstractUser já incluem:
+    # username, email, password, first_name, last_name, etc.
+    # Você pode remover first_name e last_name no settings se quiser.
+
+    def __str__(self):
+        return self.username
