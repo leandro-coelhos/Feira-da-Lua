e05da74afdbea8982ed80ee3635845ac6a514a24
Erik <erikcojho@gmail.com>
2025-11-22 17:39:02 -0300

    Implement Add Marketplace service to pass tests

--- feira_da_lua/marketplace/models.py
+++ feira_da_lua/marketplace/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class MarketPlace(models.Model):
     name = models.CharField(max_length=255)
-    marketer = models.ForeignKey("users.Marketer",on_delete=models.CASCADE)
+    marketer = models.ForeignKey("users.Marketer", blank=True, null=True, on_delete=models.SET_NULL)
     address = models.CharField(max_length=255)
     coordinates = models.CharField(max_length=255)
 
@@ -15,7 +15,7 @@ class Products(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     photo = models.BinaryField()
-    marketer = models.ForeignKey("users.Marketer",on_delete=models.CASCADE)
+    marketer = models.ForeignKey("users.Marketer", blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
--- feira_da_lua/marketplace/service.py
+++ feira_da_lua/marketplace/service.py
@@ -1,5 +1,22 @@
 from .models import MarketPlace
 from users.models import Marketer
 
-def CreateMarketPlace(name: str, marketer, address: str, coordinates: str):
-    pass
+def CreateMarketPlace(name: str, marketer: Marketer, address: str, coordinates: str) -> MarketPlace:
+    """
+    Cria um novo marketplace.
+
+    @param name: Nome do marketplace.
+    @param marketer: Objeto Marketer associado ao marketplace.
+    @param address: Endereço do marketplace.
+    @param coordinates: Coordenadas do marketplace.
+
+    @return O objeto MarketPlace criado.
+    """
+    marketplace = MarketPlace(
+        name=name,
+        marketer=marketer,
+        address=address,
+        coordinates=coordinates
+    )
+    marketplace.save()
+    return marketplace
