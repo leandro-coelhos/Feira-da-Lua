9b918aa0d638467986eace72d70a05669d43aa9b
Erik <erikcojho@gmail.com>
2025-11-22 17:33:44 -0300

    Add Marketplace service tests

---
+++ feira_da_lua/marketplace/service.py
@@ -0,0 +1,5 @@
+from .models import MarketPlace
+from users.models import Marketer
+
+def CreateMarketPlace(name: str, marketer, address: str, coordinates: str):
+    pass
--- feira_da_lua/marketplace/tests.py
+++ feira_da_lua/marketplace/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from users.models import User, Marketer
+from marketplace.service import CreateMarketPlace
 
-# Create your tests here.
+
+class MarketPlaceServiceTest(TestCase):
+    def test_create_marketplace(self):
+        # Criar user que será dono do marketer
+        user = User.objects.create(
+            email="teste@example.com",
+            username="tester",
+            password="123456",
+            complete_name="Tester da Silva"
+        )
+
+        # Criar marketer
+        marketer = Marketer.objects.create(
+            user=user,
+            cellphone="61999999999"
+        )
+
+        # Criar marketplace usando o service
+        marketplace = CreateMarketPlace(
+            name="Feira da Lua",
+            marketer=marketer,
+            address="Rua das Estrelas, 123",
+            coordinates="0,0"
+        )
+
+        # Testes
+        self.assertIsNotNone(marketplace)
+        self.assertEqual(marketplace.name, "Feira da Lua")
+        self.assertEqual(marketplace.address, "Rua das Estrelas, 123")
+        self.assertEqual(marketplace.coordinates, "0,0")
+        self.assertEqual(marketplace.marketer, marketer)
