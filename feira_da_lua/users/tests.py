92375573fb276ce04e5b3da03eba8d3dea55ab0d
Leandro <leandrocs1500@gmail.com>
2025-11-22 16:24:30 -0300

    Teste GetUser fail

--- feira_da_lua/users/tests.py
+++ feira_da_lua/users/tests.py
@@ -4,9 +4,12 @@ from .service import CreateUser
 
 class UserServiceTest(TestCase):
      def test_create_user(self):
-         user = CreateUser(email="leandrocs1500@gmail.com", username="leandrocs1500", password="securepassword", complete_name="Leandro Coelho Silva")
-         self.assertIsNotNone(user)
-     
+          user = CreateUser(email="leandrocs1500@gmail.com", username="leandrocs1500", password="securepassword", complete_name="Leandro Coelho Silva")
+          self.assertIsNotNone(user)
+     def test_get(self):
+          user = CreateUser(email="leandrocs1500@gmail.com", username="leandrocs1500", password="securepassword", complete_name="Leandro Coelho Silva")
+          self.assertEqual(user, getUserById(user.id))
+          self.assertEqual(user, getUserByEmail(user.email))
      
       
 # Create your tests here.
