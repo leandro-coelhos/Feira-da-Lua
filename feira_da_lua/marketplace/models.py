7bad00c6c4deddaa97896c78369d804c8142859c
Leandro <leandrocs1500@gmail.com>
2025-11-22 17:40:29 -0300

    README

--- feira_da_lua/marketplace/models.py
+++ feira_da_lua/marketplace/models.py
@@ -15,7 +15,7 @@ class Products(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     photo = models.BinaryField()
-    marketer = models.ForeignKey("users.Marketer",on_delete=models.CASCADE)
+    marketer = models.ForeignKey("users.Marketer", blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
