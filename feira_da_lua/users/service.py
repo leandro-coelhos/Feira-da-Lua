e9323076d1a7dd9da72707ae9a96c63f6aed6dd9
Leandro <leandrocs1500@gmail.com>
2025-11-22 16:37:12 -0300

    Test UpdateUser pass

--- feira_da_lua/users/service.py
+++ feira_da_lua/users/service.py
@@ -35,3 +35,25 @@ def GetUserByEmail(email: str) -> User:
      """
      return User.objects.get(email=email)
 
+def UpdateUser(user_id: int, username: str = None, complete_name: str = None, password: str = None) -> None:
+     """
+     Atualiza os detalhes de um usuário existente.
+
+     @param user_id: O ID do usuário a ser atualizado.
+     @param username: O novo nome de usuário (opcional).
+     @param complete_name: O novo nome completo (opcional).
+     @param password: A nova senha (opcional).
+
+     @return O objeto User atualizado.
+     """
+     user = GetUserById(user_id)
+     if username is not None:
+          user.username = username
+     if complete_name is not None:
+          user.complete_name = complete_name
+     if password is not None:
+          user.password = password
+     user.save()
+
+     return user
+
--- feira_da_lua/users/tests.py
+++ feira_da_lua/users/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .service import CreateUser, GetUserById, GetUserByEmail
+from .service import CreateUser, GetUserById, GetUserByEmail, UpdateUser
 # Service tests
 
 class UserServiceTest(TestCase):
@@ -12,8 +12,10 @@ class UserServiceTest(TestCase):
           self.assertEqual(user, GetUserByEmail(user.email))
      def test_update_user(self):
           user = CreateUser(email="leandrocs1500@gmail.com", username="leandrocs1500", password="securepassword", complete_name="Leandro Coelho Silva")
-          UpdateUser(user.id, usarnme="newusername", complete_name="New Name")
+          UpdateUser(user.id, username="newusername", complete_name="New Name", password="newpassword")
+          user = GetUserById(user.id)
           self.assertEqual(user.username, "newusername")
           self.assertEqual(user.complete_name, "New Name")
-                
+          self.assertEqual(user.password, "newpassword")
+
 # Create your tests here.
