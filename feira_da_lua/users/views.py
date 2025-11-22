a1db9080f0d19bfbc806425e16f74eb5bb8e4c40
Leandro <leandrocs1500@gmail.com>
2025-11-22 18:16:27 -0300

    Rotas users criadas

--- feira_da_lua/users/views.py
+++ feira_da_lua/users/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views import View
+
+def LoginUser(View):
+    pass
+
+def RegisterUser(View):
+    pass
+
+def LogoutUser(View):
+    pass
+
+def ProfileUser(View):
+    pass
+
+def DeleteUser(View):
+    pass
+
+def UpdateUser(View):
+    pass
+
+def ListUsers(View):
+    pass
+
+def CreateComment(View):
+    pass
+
+def DeleteComment(View):
+    pass
+
+def UpdateComment(View):
+    pass
+
+def ListComments(View):
+     pass
+
 
 # Create your views here.
